a1862d305e7f6f12f7a5c59466ee0ca6ffba7787
Liam Gray <hoxxep@gmail.com>
2024-06-24 23:02:39 +0100

    Implement StdError for mail_auth::Error

--- src/lib.rs
+++ src/lib.rs
@@ -475,6 +475,8 @@ pub enum Error {
 
 pub type Result<T> = std::result::Result<T, Error>;
 
+impl std::error::Error for Error {}
+
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
